4461efe29dc3947991060c3c449a6eae3152ac88
Colin Jaffe <colin.jaffe@gmail.com>
2024-12-12 14:00:08 -0500

    Adds proper lab file.

--- js/08.01-setInterval-setTimeout-FINAL.js
+++ js/08.01-setInterval-setTimeout-FINAL.js
@@ -100,7 +100,7 @@ const h2 = document.querySelector('h2');
 // 16. Set the **setInterval()** method equal to a variable:  
 let i = 0;
 let myInterval = setInterval(() => {
-  h2.textContent += Math.ceil(Math.random() * 100) + " ";
+  h2.textContent += " " + Math.ceil(Math.random() * 100); 
   i++;
   if(i == 10) { // when i gets to 10, clear (stop) the interval
     clearInterval(myInterval);
@@ -191,45 +191,3 @@ let animalInterval = setInterval(() => {
 // The inclusion of html in the string requires switching 
 // from textContent to innerHTML.
 // The output is too much for concatenation, so switch to backticks:
-
-// 08.01 Lab
-
-/*
-Find Digital Root
-The Digital Root of a number is the single-digit sum of its digits. 
-If the sum of the digits is 10 or more, the digits must be added 
-together again — until the result is one digit.
-
-Examples involving one, two and three "rounds" of addition:
-
-One round of addition:
-Find Digital Root of 32
-3+2 = 5
-Answer: 5
-
-Two rounds of addition:
-Find Digital Root of 84
-8+4 = 12
-1+2 = 3
-Answer: 3
-
-Three rounds of addition:
-Find Digital Root of 9,898,987
-9+8+9+8+9+8+7 = 58
-5+8 = 13
-1+3 = 4
-Answer: 4
-
-Challenge:
-Write a function called
-findDigitalRoot(n)
-which takes in a number, n,
-and returns the digital root.
-
-Bonus:
-Make a Find Digital Root DOM demo, with input box. 
-User enters a number to get its digital root. 
-The result is displayed, along with the 
-"rounds of addition", step by step.
-Hint: setInterval()
-*/
